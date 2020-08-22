7e91b77dfe15a35ec97d2b06b7f24201835328ce
Paula <paucanales@ug.uchile.cl>
2020-08-21 20:28:18 -0400

    6to commit javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,27 @@
 
 
+$('.carousel').carousel({
+  interval: 2000,
+  pause: false,
+  wrap: true
+})
+
+
+
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    
+    if (this.hash !== "") {
+      event.preventDefault();
+      
+      var hash = this.hash;
+      
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 500, function(){
+        
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
